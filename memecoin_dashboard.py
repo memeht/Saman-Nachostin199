b8fb9b66e5264d016fa5d81e5935997582ad5825
memeht <saman.nakhostin@yahoo.com>
2025-05-15 21:51:12 +0200

    Update memecoin_dashboard.py

--- memecoin_dashboard.py
+++ memecoin_dashboard.py
@@ -110,5 +110,8 @@ def dashboard():
     return render_template_string(html)
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5000)
+    if __name__ == '__main__':
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
     
